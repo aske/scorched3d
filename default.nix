20762cec4ca8a274f98619d5acc5d108490d09fc
Kirill Boltaev <aske@fmap.me>
2016-10-16 02:51:51 +0300

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,23 +1,26 @@
 { stdenv, fetchFromGitHub, mesa, glew, pkgconfig, openalSoft, freealut, wxGTK, libogg
-, freetype, libvorbis, fftwSinglePrec, SDL, SDL_net, expat, libjpeg, libpng }:
+, freetype, libvorbis, fftwSinglePrec, SDL, SDL_net, expat, libjpeg, libpng
+, perl, autoconf, automake }:
 
 stdenv.mkDerivation rec {
   version = "44";
   name = "scorched3d-${version}";
-  src = ./.;
+  src = ./scorched;
 
   buildInputs =
     [ mesa glew openalSoft freealut wxGTK libogg freetype libvorbis
       SDL SDL_net expat libjpeg libpng fftwSinglePrec
     ];
 
-  nativeBuildInputs = [ pkgconfig ];
+  nativeBuildInputs = [ pkgconfig perl autoconf automake ];
 
   sourceRoot = "scorched";
 
   configureFlags = [ "--with-fftw=${fftwSinglePrec.dev}" ];
 
   NIX_LDFLAGS = [ "-lopenal" ];
+
+  preConfigure = "sh autogen.sh";
 
   meta = with stdenv.lib; {
     homepage = http://scorched3d.co.uk/;
